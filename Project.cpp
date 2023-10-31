9aa4c07962d73a845fa383c9009e74a567719eff
Abhishek Chauhan <abhishekchauhan85805@gmail.com>
2023-10-31 13:32:43 +0530

    fourth commit

--- Project.cpp
+++ Project.cpp
@@ -26,71 +26,76 @@ struct compare {
 
 class Project{
     private:
-    Node* creatingTree(unordered_map<char, int> m, int N) {
-        priority_queue<Node*, vector<Node*>, compare> pq;
-        Node *t, *p, *q;
-        t = p = q = nullptr;
+    Node* creatingTree(unordered_map<char, int> m, int N);
+    void printing(Node* root, unordered_map<char, string> &ans, string s);
 
-        for(auto it=m.begin(); it!=m.end(); it++){
-            t = new Node(it->first, it->second);
-            pq.push(t);
-        }
-
-        while (pq.size() > 1) {
-            p = pq.top();
-            pq.pop();
-            q = pq.top();
-            pq.pop();
-            int freq = p->freq + q->freq;
-            t = new Node('\0', freq);
-            t->left = p;
-            t->right = q;
-            pq.push(t);
-        }
-
-        return pq.top();
-    }
-
-    void printing(Node* root, unordered_map<char, string> &ans, string s) {
-        if (root->left == nullptr && root->right == nullptr) {
-            ans[root->ch] = s;
-            return;
-        }
-
-        if (root->left) {
-            printing(root->left, ans, s + '0');
-        }
-        if (root->right) {
-            printing(root->right, ans, s + '1');
-        }
-    }
     public:
-        Node *root = NULL;
-        unordered_map<char, string> huffmanEncoding(unordered_map<char, int> m, int N){
-            root = creatingTree(m, N);
-            unordered_map<char, string> ans;
-            string s = "";
-            printing(root, ans, s);
-            return ans;
-        }
-
-        string decodeHuffmanData(Node* root, string binaryString){
-            string res;
-            Node *temp = root;
-            for(int i=0; i<binaryString.length(); i++){
-                if(binaryString[i] == '0') temp = temp->left;
-                else temp = temp->right;
-                
-                if(temp->left == NULL && temp->right == NULL){
-                    res += temp->ch;
-                    temp = root;
-                }
-            }
-            return res;
-        }
-
-
+    Node *root = NULL;
+    unordered_map<char, string> huffmanEncoding(unordered_map<char, int> m, int N);
+    string decodeHuffmanData(Node* root, string binaryString);
 };
+
+Node* Project::creatingTree(unordered_map<char, int> m, int N) {
+    priority_queue<Node*, vector<Node*>, compare> pq;
+    Node *t, *p, *q;
+    t = p = q = nullptr;
+
+    for(auto it=m.begin(); it!=m.end(); it++){
+        t = new Node(it->first, it->second);
+        pq.push(t);
+    }
+
+    while (pq.size() > 1) {
+        p = pq.top();
+        pq.pop();
+        q = pq.top();
+        pq.pop();
+        int freq = p->freq + q->freq;
+        t = new Node('\0', freq);
+        t->left = p;
+        t->right = q;
+        pq.push(t);
+    }
+
+    return pq.top();
+}
+
+void Project::printing(Node* root, unordered_map<char, string> &ans, string s) {
+    if (root->left == nullptr && root->right == nullptr) {
+        ans[root->ch] = s;
+        return;
+    }
+
+    if (root->left) {
+        printing(root->left, ans, s + '0');
+    }
+    if (root->right) {
+        printing(root->right, ans, s + '1');
+    }
+}
+
+unordered_map<char, string> Project::huffmanEncoding(unordered_map<char, int> m, int N){
+    root = creatingTree(m, N);
+    unordered_map<char, string> ans;
+    string s = "";
+    printing(root, ans, s);
+    return ans;
+}
+
+string Project::decodeHuffmanData(Node* root, string binaryString){
+    string res;
+    Node *temp = root;
+    for(int i=0; i<binaryString.length(); i++){
+        if(binaryString[i] == '0') temp = temp->left;
+        else temp = temp->right;
+                
+        if(temp->left == NULL && temp->right == NULL){
+            res += temp->ch;
+            temp = root;
+        }
+    }
+    return res;
+}
 
 int main(){
     string s; 
